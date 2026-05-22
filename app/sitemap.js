febe6cf4c3dface77f5538b77bf7e41f36d828ae
Alessio <alss.ldt@gmail.comm>
2026-05-22 12:24:33 +0200

    aggiornato la sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,29 @@ export default function sitemap() {
             changeFrequency: 'monthly',
             priority: 1,
         },
+        {
+            url: 'https://www.itpulizie.it/#chi-siamo',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.itpulizie.it/#servizi',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://www.itpulizie.it/#preventivo',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://www.itpulizie.it/#contatti',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.7,
+        },
     ]
 }
